b523e4dde22853f0719616d5f582b88927a17eea
oidrissi <oyazami@gmail.com>
2022-08-20 20:25:19 +0200

    Launch

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './header.css';
+import Phone from "../../assets/imgs/Iphone11Frame.png";
+import { FaBeer } from "react-icons/fa"
+
+const Header = () => {
+	return (
+		<div className="container">
+			<div className="first-column">
+				<div className="logo">
+					<img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="logo" />
+				</div>
+				<div className="phone">
+					<img src={ Phone } alt="phone" />
+				</div>
+			</div>
+			<div className="second-column">
+				<div className="search">
+					<input type="text" placeholder="Search" />
+					<button>
+						<i className={ FaBeer }></i>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
